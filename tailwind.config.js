4a9adf912c8371136333b075669c873037fce016
Joseph Scafidi <jscafidi616@hotmail.com>
2025-07-26 00:34:12 -0600

    tailwindcss updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import animate from 'tailwindcss-animate';
-
-export default {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
 	content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
 	theme: {
 		extend: {
