84da63f4915fa93efe040d62cff89ffc906b492d
Joseph Scafidi <jscafidi616@hotmail.com>
2025-08-14 10:53:52 -0600

    shadcn adaptation ThemeToggle

--- src/components/custom/ThemeToggle.tsx
+++ src/components/custom/ThemeToggle.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { Button } from '@/components/ui/button';
 import useDarkMode from '@/lib/hooks/useDarkMode.ts';
 import { cn } from '@/lib/utils/utils.ts';
 import { Moon, Sun } from 'lucide-react';
@@ -8,20 +9,29 @@ export function ThemeToggle() {
 
 	return (
 		<>
-			<button
-				type='button'
+			<Button
+				variant='ghost'
+				size='icon'
 				className={cn(
-					'hover:bg-accent  p-2 rounded-full bg-muted/30 dark:bg-neutral-700/30 dark:hover:bg-neutral-700/50 transition-colors',
+					'hover:bg-accent h-10 w-10 rounded-2xl bg-muted/30 dark:bg-neutral-700/30',
 				)}
 				onClick={toggleDark}
 				aria-label='Cambiar tema'
 			>
 				{isDark ? (
-					<Sun className='h-[1.2rem] w-[1.2rem] scale-100 transition-all bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white z-51' />
+					<Sun
+						className={cn(
+							'h-[1.2rem] w-[1.2rem] scale-100 transition-all text-gray-800 dark:text-white',
+						)}
+					/>
 				) : (
-					<Moon className='h-[1.2rem] w-[1.2rem] scale-100 transition-all bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-white z-51' />
+					<Moon
+						className={cn(
+							'h-[1.2rem] w-[1.2rem] scale-100 transition-all text-gray-800 dark:text-white',
+						)}
+					/>
 				)}
-			</button>
+			</Button>
 		</>
 	);
 }
