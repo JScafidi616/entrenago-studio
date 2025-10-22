d29fddc548264f8752deff2fc95dd888955ca591
Joseph Scafidi <jscafidi616@hotmail.com>
2025-10-21 23:49:03 -0600

    Fix auth button text color

--- src/components/custom/AuthProviders.tsx
+++ src/components/custom/AuthProviders.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button';
 import cn from 'clsx';
 
 interface AuthProvidersProps {
@@ -14,15 +15,15 @@ const AuthProviders = ({
 	authClick,
 }: AuthProvidersProps) => {
 	return (
-		<button
+		<Button
 			onClick={authClick}
 			className={cn(
-				'w-full flex items-center justify-center gap-2 py-2 px-4 border border-border rounded-lg hover:bg-muted/80 transition bg-gradient-to-r from-cyan-500 to-green-400',
+				'w-full flex bg-primary text-primary-foreground items-center justify-center font-semibold gap-2 py-2 px-4 border border-border rounded-md hover:bg-primary/90 transition bg-gradient-to-r from-cyan-500 to-green-400',
 			)}
 		>
 			<img src={providerImage} alt={providerName} className={cn('w-5 h-5')} />
 			<span className={cn('text-sm font-semibold')}>{providerDescription}</span>
-		</button>
+		</Button>
 	);
 };
 
