d9af8f583b2f2902b0d04556adaad8b82e73bf20
Joseph Scafidi <jscafidi616@hotmail.com>
2026-06-23 12:12:11 -0600

    Loader component added

---
+++ src/components/loader/Loader.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface LoaderProps {
+	message: string;
+}
+
+const Loader: React.FC<LoaderProps> = ({ message }) => (
+	<div className='loader'>
+		<span className='loader-text'>
+			<p style={{ fontSize: '1rem', fontWeight: 500 }}>{message}</p>
+		</span>
+		<span className='load'></span>
+	</div>
+);
+
+export default Loader;
