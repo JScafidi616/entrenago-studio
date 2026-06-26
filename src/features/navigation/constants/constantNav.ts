8922db50be693ef7d286178e97a1adab1495dd1c
Joseph Scafidi <jscafidi616@hotmail.com>
2026-06-25 23:15:55 -0600

    Added constant var shared

---
+++ src/features/navigation/constants/constantNav.ts
@@ -0,0 +1,8 @@
+import { Dumbbell, Home, TrendingUp } from 'lucide-react';
+
+//Add more possible routes from this section...
+export const navItems = [
+  { id: '/dashboard', label: 'Dashboard', icon: Home },
+  { id: '/progress-tracking', label: 'Progress', icon: TrendingUp },
+  { id: '/my-routines', label: 'Routines', icon: Dumbbell },
+];
