a4a5b2e78fa5d93e50bde3b4f5843507cf70bb26
Joseph Scafidi <jscafidi616@hotmail.com>
2026-06-26 00:12:03 -0600

    Added optimization for motion

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { App } from '@/App';
 import { AuthProvider } from '@/context/AuthContext.tsx';
 import '@/styles/global.css';
+import { LazyMotion, domMax } from 'motion/react';
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
@@ -19,7 +20,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
 		<QueryClientProvider client={queryClient}>
 			<AuthProvider>
-				<App />
+				<LazyMotion features={domMax}>
+					<App />
+				</LazyMotion>
 			</AuthProvider>
 			<ReactQueryDevtools initialIsOpen={false} />
 		</QueryClientProvider>
