28e1c912c978d533de504a1ebe8e8ddca8047e33
Joseph Scafidi <jscafidi616@hotmail.com>
2026-06-26 15:01:54 -0600

    Added new route for account path

--- src/features/navigation/constants/constantNav.ts
+++ src/features/navigation/constants/constantNav.ts
@@ -1,8 +1,13 @@
-import { Dumbbell, Home, TrendingUp } from 'lucide-react';
+import { Dumbbell, Home, TrendingUp, Settings, UserCircle } from 'lucide-react';
 
 //Add more possible routes from this section...
-export const navItems = [
+export const appNavItems = [
   { id: '/dashboard', label: 'Dashboard', icon: Home },
   { id: '/progress-tracking', label: 'Progress', icon: TrendingUp },
   { id: '/my-routines', label: 'Routines', icon: Dumbbell },
+];
+
+export const accountNavItems = [
+  { id: '/profile', label: 'Profile', icon: UserCircle },
+  { id: '/settings', label: 'Settings', icon: Settings }
 ];
