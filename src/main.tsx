7984e25623fcce15806c3dcfb2cf631f5be47c9c
Joseph Scafidi <jscafidi616@hotmail.com>
2026-06-26 23:12:52 -0600

    update query client

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,14 @@
 // src/main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { QueryClientProvider } from '@tanstack/react-query';
 import { App } from '@/App';
 import { AuthProvider } from '@/context/AuthContext.tsx';
 import '@/styles/global.css';
 import { LazyMotion, domMax } from 'motion/react';
+import { queryClient } from '@/lib/queryClient'; // Adjust path
 
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-
-// Crear una instancia del cliente de React Query
-const queryClient = new QueryClient({
-	defaultOptions: {
-		queries: { staleTime: 1000 * 60 * 5, retry: 1 },
-	},
-});
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
 	<React.StrictMode>
