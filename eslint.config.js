6ab2a65c2f2fc7c4c7c04bed95ec625ce638cb46
Joseph Scafidi <jscafidi616@hotmail.com>
2026-07-02 18:57:11 -0600

    Added prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,6 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
 import storybook from 'eslint-plugin-storybook';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 import js from '@eslint/js';
 import globals from 'globals';
@@ -30,4 +31,5 @@ export default tseslint.config(
 		},
 	],
 	storybook.configs['flat/recommended'],
+	eslintConfigPrettier,
 );
