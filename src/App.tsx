188f8e49327f4fe5c31dfaa89655680be879ac17
Joseph Scafidi <jscafidi616@hotmail.com>
2026-07-06 11:32:01 -0600

    Added main Toaster component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 // src/App.tsx
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/routes';
-// import { Toaster } from 'react-hot-toast';
+import { Toaster } from 'sonner';
 
 export const App = () => {
 	return (
 		<>
 			{/* Global UI elements that persist across all routes */}
-			{/* <Toaster position="top-right" /> */}
-
+			{/* Toaster component */}
+			<Toaster position="bottom-center" richColors />
+			{/* Routing component */}
 			<RouterProvider router={router} />
 		</>
 	);
