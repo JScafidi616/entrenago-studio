a994f6ed490085ef272225544112f134d1ffa167
Joseph Scafidi <jscafidi616@hotmail.com>
2026-07-07 00:26:07 -0600

    Upgraded props to support mutation change

--- src/features/auth/components/AuthProviders.tsx
+++ src/features/auth/components/AuthProviders.tsx
@@ -3,12 +3,14 @@ import cn from 'clsx';
 
 interface AuthProvidersProps {
 	providerName: string;
+	disabled: boolean;
 	providerDescription: string;
 	providerImage: string;
 	authClick: () => void;
 }
 
 const AuthProviders = ({
+	disabled,
 	providerName,
 	providerDescription,
 	providerImage,
@@ -16,6 +18,7 @@ const AuthProviders = ({
 }: AuthProvidersProps) => {
 	return (
 		<Button
+			disabled={disabled}
 			onClick={authClick}
 			className={cn(
 				'bg-primary text-primary-foreground border-border hover:bg-primary/90 flex w-full items-center justify-center gap-2 rounded-md border bg-linear-to-r from-cyan-500 to-green-400 px-4 py-2 font-semibold transition',
